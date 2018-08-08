eff6308a63e1649f978213dcb06414492c1a18b6
TCJ23 <tcj2012@gmail.com>
2018-08-08 13:29:22 +0200

    testing coverage

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,4 +1,11 @@
 public class Hello {
+    public static void testing() {
+        System.out.println("TEST");
+        System.out.println("TEST");
+        System.out.println("TEST");
+        System.out.println("TEST");
+        System.out.println("TEST");
+    }
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("SONAR");
---
+++ src/test/java/HelloTest.java
@@ -0,0 +1,9 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloTest {
+
+    @org.junit.jupiter.api.Test
+    void testing() {
+    Hello.testing();
+    }
+}
