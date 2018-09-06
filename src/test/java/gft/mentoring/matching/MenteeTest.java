1868c090156e3c53964292d4c4f0e58b5a1f9758
TCJ23 <tcj2012@gmail.com>
2018-09-06 11:17:59 +0200

    refactoring of Test Clasess to exclude hardcoded numbers in Assertion methods

--- src/test/java/gft/mentoring/matching/MenteeTest.java
+++ src/test/java/gft/mentoring/matching/MenteeTest.java
@@ -12,6 +12,7 @@ public class MenteeTest {
     //Given
     private static List<Mentee> menteeList;
     private static List<Mentor> mentorList;
+    private static int countMtr = 0;
 
 
     //Given
@@ -20,41 +21,42 @@ public class MenteeTest {
         MenteeTest menteeTest = new MenteeTest();
         menteeTest.setMentorList();
         menteeTest.setMenteeList();
+        menteeTest.countDevFamMtr();
     }
 
     //When
     @Test
-    public void getMentorsForDevMenteeShouldFind4() {
+    public void getMentorsForDevelopmentMenteeShouldFind4() {
         int mentorsNumber = menteeList.get(0).getMentors(mentorList).size();
         //Then
-        assertEquals(4, mentorsNumber);
+        assertEquals(countMtr, mentorsNumber);
         System.out.println("In our data set we have 4 potential mentors for any mentee from Development Family");
     }
 
     private void setMenteeList() {
         menteeList = new ArrayList<>();
-        System.out.println("We create 6 GFT mentees, 4 of them are part of larger development family");
-        Mentee mentee = new Mentee(1,  1,Family.DEVELOPMENT);
+        System.out.println("We created 6 GFT mentees, 4 of them are part of larger development family");
+        Mentee mentee = new Mentee(1, 1, Family.DEVELOPMENT);
         menteeList.add(mentee);
-        Mentee mentee2 = new Mentee(2,  2,Family.ARCHITECTURE);
+        Mentee mentee2 = new Mentee(2, 2, Family.ARCHITECTURE);
         menteeList.add(mentee2);
-        Mentee mentee3 = new Mentee(3,  3,Family.DATA);
+        Mentee mentee3 = new Mentee(3, 3, Family.DATA);
         menteeList.add(mentee3);
-        Mentee mentee4 = new Mentee(4,  2,Family.DIGITAL);
+        Mentee mentee4 = new Mentee(4, 2, Family.DIGITAL);
         menteeList.add(mentee4);
-        Mentee mentee5 = new Mentee(5,  3,Family.OTHER);
+        Mentee mentee5 = new Mentee(5, 3, Family.OTHER);
         menteeList.add(mentee5);
-        Mentee mentee6 = new Mentee(6,  1,Family.OTHER);
+        Mentee mentee6 = new Mentee(6, 1, Family.OTHER);
     }
 
     private void setMentorList() {
         mentorList = new ArrayList<>();
-        System.out.println("We create 6 GFT mentors, 4 of them are part of larger development family");
+        System.out.println("We created 6 GFT mentors, 4 of them are part of larger development family");
         Mentor mentor = new Mentor(1, 4, Family.DEVELOPMENT, 6);
         mentorList.add(mentor);
         Mentor mentor2 = new Mentor(2, 5, Family.ARCHITECTURE, 4);
         mentorList.add(mentor2);
-        Mentor mentor3 = new Mentor(3, 6, Family.DATA,6);
+        Mentor mentor3 = new Mentor(3, 6, Family.DATA, 6);
         mentorList.add(mentor3);
         Mentor mentor4 = new Mentor(4, 5, Family.DIGITAL, 6);
         mentorList.add(mentor4);
@@ -62,5 +64,14 @@ public class MenteeTest {
         mentorList.add(mentor5);
         Mentor mentor6 = new Mentor(6, 5, Family.OTHER, 2);
         mentorList.add(mentor6);
+    }
+
+    private void countDevFamMtr() {
+        for (Mentor mtr : mentorList
+                ) {
+            if (mtr.checkIfDev()) {
+                countMtr++;
+            }
+        }
     }
 }
--- src/test/java/gft/mentoring/matching/MentorTest.java
+++ src/test/java/gft/mentoring/matching/MentorTest.java
@@ -6,36 +6,38 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MentorTest {
+
     List<Mentee> menteeList = new ArrayList<>();
     List<Mentor> mentorList = new ArrayList<>();
+    int countMnt = 0;
 
     @BeforeEach
     public void setMenteeList() {
-        System.out.println("We create 6 GFT mentees, 5 of them are part of larger development family");
-        Mentee mentee = new Mentee(1,  1,Family.DEVELOPMENT);
+        System.out.println("We created 6 GFT mentees, 5 of them are part of larger development family");
+        Mentee mentee = new Mentee(1, 1, Family.DEVELOPMENT);
         menteeList.add(mentee);
-        Mentee mentee2 = new Mentee(2,  2,Family.ARCHITECTURE);
+        Mentee mentee2 = new Mentee(2, 2, Family.ARCHITECTURE);
         menteeList.add(mentee2);
-        Mentee mentee3 = new Mentee(3,  3,Family.DATA);
+        Mentee mentee3 = new Mentee(3, 3, Family.DATA);
         menteeList.add(mentee3);
-        Mentee mentee4 = new Mentee(4,  2,Family.DIGITAL);
+        Mentee mentee4 = new Mentee(4, 2, Family.DIGITAL);
         menteeList.add(mentee4);
-        Mentee mentee5 = new Mentee(5,  3,Family.DEVELOPMENT);
+        Mentee mentee5 = new Mentee(5, 3, Family.DEVELOPMENT);
         menteeList.add(mentee5);
-        Mentee mentee6 = new Mentee(6,  1,Family.OTHER);
+        Mentee mentee6 = new Mentee(6, 1, Family.OTHER);
     }
 
     @BeforeEach
     public void setMentorList() {
-        System.out.println("We create 6 GFT mentors, 4 of them are part of larger development family");
+        System.out.println("We created 6 GFT mentors, 4 of them are part of larger development family");
         Mentor mentor = new Mentor(1, 4, Family.DEVELOPMENT, 6);
         mentorList.add(mentor);
         Mentor mentor2 = new Mentor(2, 5, Family.ARCHITECTURE, 4);
         mentorList.add(mentor2);
-        Mentor mentor3 = new Mentor(3, 6, Family.DATA,6);
+        Mentor mentor3 = new Mentor(3, 6, Family.DATA, 6);
         mentorList.add(mentor3);
         Mentor mentor4 = new Mentor(4, 5, Family.DIGITAL, 6);
         mentorList.add(mentor4);
@@ -44,10 +46,22 @@ class MentorTest {
         Mentor mentor6 = new Mentor(6, 5, Family.OTHER, 2);
         mentorList.add(mentor6);
     }
+
+    @BeforeEach
+    public void countDevFamMnt() {
+        for (Mentee mnt : menteeList
+                ) {
+            if (mnt.checkIfDev()) {
+                countMnt++;
+            }
+        }
+    }
+
+
     @Test
-    void getMenteesForDevMentorShouldFind5() {
+    void getMenteesForDevelopmentMentorShouldFind5() {
         int menteesNumber = mentorList.get(3).getMentees(menteeList).size();
-        assertEquals(5,menteesNumber);
+        assertEquals(countMnt, menteesNumber);
         System.out.println("In our data set we have 5 potential mentees for any mentor in Development Family");
     }
 }
