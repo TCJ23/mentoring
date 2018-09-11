d54a32219ec35866faa5adde53804d0721f34175
TCJ23 <tcj2012@gmail.com>
2018-09-11 14:15:17 +0200

    small refactoring of test method and dependencies in pom

--- src/test/java/gft/mentoring/matching/TestMatching.java
+++ src/test/java/gft/mentoring/matching/TestMatching.java
@@ -6,32 +6,51 @@ import gft.mentoring.matching.model.Mentor;
 import lombok.Value;
 import lombok.val;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
+
 import java.util.stream.Stream;
 
-
+/*
+@author tzje
+* This class should test MatchingEngine().findProposals */
 public class TestMatching {
 
-    @ParameterizedTest
-    @MethodSource
+    /* in @ParameterizedTest you either keep
+    your testmethod name and method source name the same or use parameters as below*/
+
+    @ParameterizedTest(name = "{0}")
+    @MethodSource("singleMatchingParam")
+    @DisplayName("Check if MatchingEngine triages mentors per Family")
     void findSingleCandidateMentorFromProposals(SingleMatchingParam singleMatchingParam) {
+        //given
         val candidate = new Mentor(1, singleMatchingParam.mentorCandidateFamily);
-        val mentee = new Mentee(1,  singleMatchingParam.menteeFamily);
+        val mentee = new Mentee(1, singleMatchingParam.menteeFamily);
+        //when
         val proposals = new MatchingEngine().findProposals(mentee, candidate);
+        //then
         Assertions.assertThat(proposals.size() == 1).isEqualTo(singleMatchingParam.accepted);
     }
 
-    static Stream<SingleMatchingParam> findSingleCandidateMentorFromProposals() {
+    static Stream<SingleMatchingParam> singleMatchingParam() {
         return Stream.of(
-                new SingleMatchingParam(Family.PROJECT_DEVELOPMENT, Family.PROJECT_DEVELOPMENT, true),
-                new SingleMatchingParam(Family.ARCHITECTURE, Family.CORPORATE_SERVICES, false));
+                new SingleMatchingParam("Scenario: Mentor & Mentee are both in Development Group",
+                                        Family.PROJECT_DEVELOPMENT, Family.PROJECT_DEVELOPMENT, true),
+                new SingleMatchingParam("Scenario: Mentor & Mentee are NOT in Development Group",
+                                        Family.ARCHITECTURE, Family.CORPORATE_SERVICES, false));
     }
 
     @Value
     static class SingleMatchingParam {
+        private String scenario;
         private Family menteeFamily;
         private Family mentorCandidateFamily;
         private boolean accepted;
+
+        @Override
+        public String toString() {
+            return scenario;
+        }
     }
 }
