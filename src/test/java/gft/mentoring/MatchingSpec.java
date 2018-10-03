eb7afe502ce08335e5c2d39926a5ecd03f0fff87
TCJ23 <tcj2012@gmail.com>
2018-10-03 10:07:18 +0200

    Issue#11  req 1.14  add GREEN UNIT TEST for preferring Mentor with higher Level if Seniority is Equal

--- src/test/java/gft/mentoring/MatchingSpec.java
+++ src/test/java/gft/mentoring/MatchingSpec.java
@@ -336,17 +336,35 @@ class MatchingSpec {
     void shouldPreferDevManWithHigherSeniorityInsteadofMentorWithHigherGrade() {
         //given
         val mentee = newMentee().build();
-        val highLevelMentor = newMentor().seniority(1 * 365).level(6).build();
+        val highLevelMentor = newMentor().seniority(1 * 365).level(5).build();
         val highSeniorityMentor = newMentor().seniority(3 * 365).level(5).build();
-        val highestSeniortiyMentor = newMentor().seniority(4 * 365).level(4).build();
-        val highestLevelMentor = newMentor().seniority(3 * 365).level(5).build();
+        val highestSeniorityMentor = newMentor().seniority(4 * 365).level(4).build();
+        val highestLevelMentor = newMentor().seniority(3 * 365).level(6).build();
         //when
         val proposals = new MatchingEngine().findProposals(mentee, highLevelMentor, highSeniorityMentor,
-                highestSeniortiyMentor, highestLevelMentor).collect(Collectors.toList());
+                highestSeniorityMentor, highestLevelMentor).collect(Collectors.toList());
         //then
         val proposedMentor = proposals.get(0);
         System.out.println(proposedMentor);
-        assertThat(proposedMentor.equals(highestSeniortiyMentor)).isTrue();
+        assertThat(proposedMentor.equals(highestSeniorityMentor)).isTrue();
+    }
+
+    @Test
+    @DisplayName("1.14 - Equal Seniority then prefer Level")
+    void shouldPreferDevManWithHigherLevelWhenSeniorytIsEqual() {
+        //given
+        val mentee = newMentee().build();
+        val highLevelMentor = newMentor().seniority(1 * 365).level(6).build();
+        val fourYearsSeniorityLevel5 = newMentor().seniority(4 * 365).level(5).build();
+        val fourYearsSeniorityLevel4 = newMentor().seniority(4 * 365).level(4).build();
+        val highestLevelMentor = newMentor().seniority(3 * 365).level(5).build();
+        //when
+        val proposals = new MatchingEngine().findProposals(mentee, highLevelMentor, fourYearsSeniorityLevel5,
+                fourYearsSeniorityLevel4, highestLevelMentor).collect(Collectors.toList());
+        //then
+        val proposedMentor = proposals.get(0);
+        System.out.println(proposedMentor);
+        assertThat(proposedMentor.equals(fourYearsSeniorityLevel5)).isTrue();
     }
 
     @Test
