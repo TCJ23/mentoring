38d8d75db9ca15c40e83accd2bc1aab355e9cddc
TCJ23 <tcj2012@gmail.com>
2018-10-22 14:01:30 +0200

    Issue#19  REQ 4.1  TRS Model and description  red unit test to read TRS Sample file

--- src/main/java/gft/mentoring/trs/model/TRSInput.java
+++ src/main/java/gft/mentoring/trs/model/TRSInput.java
@@ -1,11 +1,39 @@
 package gft.mentoring.trs.model;
 
 
+import lombok.val;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
 import java.util.List;
 
 class TRSInput {
 
     List<TRSModel> readExcelTRSfile(String trsFile) {
-        return null;
+        Workbook workbook;
+
+        try {
+            workbook = WorkbookFactory.create(new File(trsFile));
+            Sheet sheet = workbook.getSheetAt(0);
+            Iterator<Row> iterator = sheet.iterator();
+            /* first row is simply names of columns*/
+            iterator.next();
+            val result = readRows(iterator);
+            workbook.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InvalidFormatException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    private List<TRSModel> readRows(Iterator<Row> iterator) {
     }
 }
--- src/main/java/gft/mentoring/trs/model/TRSModel.java
+++ src/main/java/gft/mentoring/trs/model/TRSModel.java
@@ -1,4 +1,48 @@
 package gft.mentoring.trs.model;
 
-public class TRSModel {
+import gft.mentoring.Family;
+import gft.mentoring.MentoringModel;
+import gft.mentoring.sap.model.SAPMentoringModel;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author tzje
+ * This model should be created while reading excel file delivered by TRS only from valid and useful fields
+ */
+@NoArgsConstructor
+@Data
+class TRSModel {
+    /** At the time of writing only below fields hold useful information
+     * @param name - will be used for Merging TRS and SAP models into final MentoringModel
+     * @param surname - will be used for Merging TRS and SAP models into final MentoringModel
+     * @param status - this field appears to hold intel about if GFT person is a leaver, only
+     *               Employee can be considered as candidate in MentoringModel
+     *               Hired will be ignored
+     *               Notice Period - marks person as Leaver
+     * @see MentoringModel#isLeaver()
+     * @param grade - level
+     * @see MentoringModel#getLevel()
+     * @param technology - specialization
+     * @see MentoringModel#getSpecialization()
+     * @param jobFamily - simply Family of GFT person
+     * @see Family
+     * @param startDate - this field appears to hold intel about how long person is working at GFT company
+     * @see MentoringModel#getSeniority()
+     * @param officeLocation
+     * @see MentoringModel#getLocalization()
+     * @param contractType - this param should match between TRS & SAP models
+     * @see MentoringModel#isContractor()
+     * &&
+     * @see SAPMentoringModel#isContractor()
+     * */
+    private String name;
+    private String surname;
+    private String status;//contains also leaver (is or not contractor)
+    private String grade;
+    private String technology;
+    private String jobFamily;
+    private String startDate;
+    private String officeLocation;
+    private String contractType;
 }
